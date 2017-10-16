051d3560a705d061ee6ca6366ef1d4db6bcb146f
DenisSkygate <denis@skygate.be>
2017-10-16 08:12:18 +0200

    Xamarin Alliance and Master Detail Sample added

---
+++ MasterDetailSample/MasterDetailSample/MasterDetailSample/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MasterDetailSample
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
