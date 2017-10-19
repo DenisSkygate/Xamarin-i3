83b3e44f4d59b6abbfde67c62d0f4033aa80d6c8
Denis Omelcenco <denis@skygate.be>
2017-10-19 16:56:59 +0200

    Account Completion - Work in progress

--- GeeCoeur/GeeCoeur/Views/Account/AccountInterestsPage.xaml.cs
+++ GeeCoeur/GeeCoeur/Views/Account/AccountInterestsPage.xaml.cs
@@ -11,5 +11,16 @@ namespace GeeCoeur.Views.Account
         {
             InitializeComponent();
         }
+        void Next_Clicked(object sender, System.EventArgs e)
+        {
+            var papa = (this.Parent as TabbedPage);
+            papa.CurrentPage = papa.Children[papa.Children.IndexOf(this) + 1];
+        }
+
+        void Back_Clicked(object sender, System.EventArgs e)
+        {
+            var papa = (this.Parent as TabbedPage);
+            papa.CurrentPage = papa.Children[papa.Children.IndexOf(this) - 1];
+        }
     }
 }
--- GeeCoeur/GeeCoeur/Views/Account/AccountSkillsPage.xaml.cs
+++ GeeCoeur/GeeCoeur/Views/Account/AccountSkillsPage.xaml.cs
@@ -7,9 +7,29 @@ namespace GeeCoeur.Views.Account
 {
     public partial class AccountSkillsPage : ContentPage
     {
+       
+
         public AccountSkillsPage()
         {
             InitializeComponent();
+        }
+        void Next_Clicked(object sender, System.EventArgs e)
+        {
+            var papa = (this.Parent as TabbedPage);
+            papa.CurrentPage = papa.Children[papa.Children.IndexOf(this) + 1];
+        }
+
+        void Back_Clicked(object sender, System.EventArgs e)
+        {
+            var papa = (this.Parent as TabbedPage);
+            papa.CurrentPage = papa.Children[papa.Children.IndexOf(this) - 1];
+        }
+        void AddSkill_Clicked(object sender, System.EventArgs e)
+        {
+            var lab = new Label();
+            lab.Text = SkillEntry.Text;
+            this.SkillsLayout.Children.Add(lab);
+            SkillEntry.Text = "";
         }
     }
 }
