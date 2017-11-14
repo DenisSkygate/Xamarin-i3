d7e89bff7f3a8e961ee661d0368029d034368439
Denis Omelcenco <denis@skygate.be>
2017-11-14 14:11:33 +0100

    Some design

--- GeeCoeur/GeeCoeur/Views/Account/AccountPage.xaml.cs
+++ GeeCoeur/GeeCoeur/Views/Account/AccountPage.xaml.cs
@@ -8,31 +8,7 @@ namespace GeeCoeur.Views.Account
 {
     public partial class AccountPage : ContentPage
     {
-        async void Handle_Clicked(object sender, System.EventArgs e)
-        {
-            PicButton.IsEnabled = false;
-            Stream stream = await DependencyService.Get<IPicturePicker>().GetImageStreamAsync();
-
-            if (stream != null)
-            {
-                Image image = new Image
-                {
-                    Source = ImageSource.FromStream(() => stream),
-                    BackgroundColor = Color.Gray
-                };
-                TapGestureRecognizer recognizer = new TapGestureRecognizer();
-                recognizer.Tapped += (sender2, args) =>
-                {
-                };
-                image.GestureRecognizers.Add(recognizer);
-
-                ProfilePic.Children.Add(image);
-            }
-            else
-            {
-                PicButton.IsEnabled = true;
-            }
-        }
+        
         public AccountPage()
         {
             InitializeComponent();
